35783e99af2412015351579ffa9da1128a9cf79f
Giovanni Cifuentes <giovannicifuentescss20@gmail.com>
2023-02-28 20:59:02 -0600

    Prueba para saber si se sube bien al repositorio (es una prueba)

---
+++ Archivo Prueva Subida/pruebaSubida.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a1, b1, total;
+    cout << "Ingrese dos numeros: "<<endl;
+    cin>>a1;
+    cin>>b1;
+    total = a1 + b1;
+    cout <<"El total es igual a: "<<total<<endl;
+    return 0;
+}
+
+
